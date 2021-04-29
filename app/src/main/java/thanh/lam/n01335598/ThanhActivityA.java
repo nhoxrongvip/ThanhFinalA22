d45ca919a61af85f1763a26e759b660d7ef592cf
nhoxrongvip <phatlam.media.vis@gmail.com>
2021-04-29 18:09:07 -0400

    Initial commit

---
+++ app/src/main/java/thanh/lam/n01335598/ThanhActivityA.java
@@ -0,0 +1,15 @@
+//Thanh Phat Lam N01335598
+package thanh.lam.n01335598;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThanhActivityA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
